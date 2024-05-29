70da1c371e48484b168608cc11b60194c9d974c2
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-05-29 14:20:58 +0500

    Update SportsFrag.java

--- app/src/main/java/com/example/create_keyboard1/fragments/SportsFrag.java
+++ app/src/main/java/com/example/create_keyboard1/fragments/SportsFrag.java
@@ -27,7 +27,7 @@ public class SportsFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    
         final  View view=inflater.inflate(R.layout.fragment_sports, container, false);
         recyclerView=view.findViewById(R.id.recyclerview);
 
