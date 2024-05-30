b2784314962e96d44b3327908efe7727cfc65bca
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-05-30 14:23:20 +0500

    Update BitmapClass.java

--- app/src/main/java/com/example/create_keyboard1/otherclasses/BitmapClass.java
+++ app/src/main/java/com/example/create_keyboard1/otherclasses/BitmapClass.java
@@ -85,15 +85,12 @@ public class BitmapClass {
     public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
                                                          int reqWidth, int reqHeight) {
 
-        // First decode with inJustDecodeBounds=true to check dimensions
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeResource(res, resId, options);
 
-        // Calculate inSampleSize
         options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
-        // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeResource(res, resId, options);
     }
@@ -109,9 +106,6 @@ public class BitmapClass {
 
             final int halfHeight = height / 2;
             final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
             while ((halfHeight / inSampleSize) >= reqHeight
                     && (halfWidth / inSampleSize) >= reqWidth) {
                 inSampleSize *= 2;
